36368604310b79aaa20ab3d15d1a12cc6046c4b0
Macauley Lim <xmachd@gmail.com>
2019-04-03 19:31:21 +1100

    Make basic web available

--- ChatWebClient/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatWebClient.MvcApplication" Language="C#" %>
---
+++ ChatWebClient/Models/ChatContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ChatWebClient.Models
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext() : base("MainDB")
+        {
+        }
+
+        public static ChatContext Create()
+        {
+            return new ChatContext();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
